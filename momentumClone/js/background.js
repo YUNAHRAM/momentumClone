123a5346c0ff3c97bfedbe42eb017030eb9e16bd
YUNAHRAM <woojookong@naver.com>
2023-02-28 12:54:54 +0900

    no message

--- momentumClone/js/background.js
+++ momentumClone/js/background.js
@@ -3,7 +3,12 @@ const images =[
     "img02.jpg",
     "img03.jpg",
     "img04.jpeg",
-    "img05.jpeg"
+    "img05.jpeg",
+    "img06.jpeg",
+    "img07.jpeg",
+    "img08.jpeg",
+    "img09.jpeg"
+
    
 ];
 const BG = "background"
